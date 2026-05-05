33616be3fe2984877d66052c8a5540d6e1269d47
汉堡包 <syahirfan545@gmail.com>
2026-05-05 10:41:58 +0700

    Implement main function to demonstrate mahasiswa class usage with pointers

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -11,3 +11,13 @@ public:
     }
 };
 
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // Pointer to Object mhs; Membuat pointer mhs yang menunjuk ke objek mahasiswa dengan nim 1
+    mhs->showNim(); // Arrow Operator; Menampilkan nim mhs melalui pointer
+    mhs->nim = 2; // Arrow Operator; Mengubah nim melalui pointer
+    mhs->showNim(); // Arrow Operator; Menampilkan nim mhs setelah diubah melalui pointer
+
+    delete mhs; // Menghapus objek yang dibuat dengan new untuk menghindari memory leak
+    return 0;
+}
