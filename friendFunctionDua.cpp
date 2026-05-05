37b11283fd93f7750a23d529a1405631f05786f2
汉堡包 <syahirfan545@gmail.com>
2026-05-05 11:01:06 +0700

    Refactor manusia class declaration and friend function for pelajar access

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -3,3 +3,9 @@ using namespace std; // Menggunakan namespace std untuk menghindari penulisan st
 
 class pelajar; // Forward declaration untuk kelas pelajar agar dapat digunakan dalam deklarasi kelas manusia
 
+class manusia // Deklarasi kelas manusia yang memiliki fungsi friend untuk mengakses anggota data private dari kelas pelajar
+{
+public: // Deklarasi fungsi friend yang dapat mengakses anggota data private dari kelas pelajar
+    void showNilaiPelajar(pelajar &p); // Fungsi untuk menampilkan nilai pelajar yang akan diakses melalui objek manusia
+};
+
