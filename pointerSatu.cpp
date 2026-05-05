156a34196506c4cd5287b573285a9f535c817e60
汉堡包 <syahirfan545@gmail.com>
2026-05-05 10:35:06 +0700

    Implement main function to demonstrate mahasiswa class usage with references and pointers

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -11,3 +11,19 @@ public:
     }
 };
 
+int main()
+{
+    mahasiswa mhs{1}; // Object mhs; Membuat objek mhs dengan nim 1
+    mhs.showNim(); // Member Accsess Operator; Menampilkan nim mhs
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs; Membuat reference refMhs yang merujuk ke mhs
+    refMhs.nim = 2; // Member Access Operator; Mengubah nim melalui reference
+    refMhs.showNim(); // Member Access Operator; Menampilkan nim melalui reference
+    mhs.showNim(); // Member Access Operator; Menampilkan nim mhs setelah diubah melalui reference
+
+    mahasiswa *pMhs = &mhs; // Pointer Dereference pMhs; Membuat pointer pMhs yang menunjuk ke mhs
+    pMhs->nim = 3; // Arrow Operator; Mengubah nim melalui pointer
+    pMhs->showNim(); // Arrow Operator; Menampilkan nim melalui pointer
+
+    return 0;
+}
