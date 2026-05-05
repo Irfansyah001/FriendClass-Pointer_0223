577840f9982836720d6db202828e1a9f25544133
汉堡包 <syahirfan545@gmail.com>
2026-05-05 11:00:43 +0700

    Add forward declaration for pelajar class in friendFunctionDua.cpp

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -1,3 +1,5 @@
 #include <iostream> // Menyertakan library iostream untuk input dan output
 using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum setiap penggunaan elemen dari library standar
 
+class pelajar; // Forward declaration untuk kelas pelajar agar dapat digunakan dalam deklarasi kelas manusia
+
