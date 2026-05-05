911e2a4784799cc7ac6bf03c3a00a65b2bfa1af0
汉堡包 <syahirfan545@gmail.com>
2026-05-05 12:23:48 +0700

    Add initial implementation of Layang-Layang and Belah Ketupat classes with methods for area and perimeter

---
+++ bangunDatar.cpp
@@ -0,0 +1,10 @@
+/*
+Buatlah sebuah program yang berisi 2 class dimana terdapat class Layang-Layang, Belah Ketupat.
+Di setiap class nya berisikan method luas dan keliling serta method input dan output.
+Setiap variable memiliki akses private, BangunDatar Belah Ketupat merupakan friend method keliling
+dari Layang-Layang, dan semua class hanya dapat diakses menggunakan object.
+*/
+
+#include <iostream> // Menyertakan library iostream untuk input dan output
+using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum setiap penggunaan elemen dari library standar
+
