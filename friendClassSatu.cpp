0222db9e1ae5ca08dffe262b96999ba63cf827ad
汉堡包 <syahirfan545@gmail.com>
2026-05-05 11:19:24 +0700

    Add initial implementation of friendClassSatu.cpp with necessary includes

---
+++ friendClassSatu.cpp
@@ -0,0 +1,4 @@
+#include <iostream> // Menyertakan library iostream untuk input dan output
+#include <string> // Menyertakan library string untuk menggunakan tipe data string
+using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum setiap penggunaan elemen dari library standar
+
