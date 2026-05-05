19919c61c73be6d31312946edf2db1b91a0e3aed
汉堡包 <syahirfan545@gmail.com>
2026-05-05 10:50:15 +0700

    Add mahasiswa class with private nama attribute and friend function to set nama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -2,3 +2,12 @@
 #include <string> // Menyertakan library string untuk menggunakan tipe data string
 using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum setiap penggunaan elemen dari library standar
 
+class mahasiswa // Deklarasi kelas mahasiswa
+{
+private: // Anggota data private untuk menyimpan nama mahasiswa
+    string nama; // Variabel untuk menyimpan nama mahasiswa
+
+public: // Deklarasi fungsi friend yang dapat mengakses anggota data private
+    friend void setNama(mahasiswa &a, string); // Fungsi friend untuk mengatur nama mahasiswa
+};
+
