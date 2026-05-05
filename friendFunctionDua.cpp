980b72865ace167eabd5db3d8c2d752826099c36
汉堡包 <syahirfan545@gmail.com>
2026-05-05 11:00:30 +0700

    Add initial includes and using directive in friendFunctionDua.cpp

---
+++ friendFunctionDua.cpp
@@ -0,0 +1,3 @@
+#include <iostream> // Menyertakan library iostream untuk input dan output
+using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum setiap penggunaan elemen dari library standar
+
